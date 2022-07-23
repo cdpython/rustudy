6c625f7de856e0ec29124c679140ec19ecdee935
cdpython <cdpython.cat@gmail.com>
2022-07-23 11:02:26 +0000

    guess

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ https://rinthel.github.io/rust-lang-book-ko/ 참고
 ```
 cargo new guess
 ```
+의존성 추가
+"guess/Cargo.toml"
+```
+...
+
+[dependencies]
+rand = "0.8.4"
+```
--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,3 +1,34 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing Game!");
+    let mut rng = rand::thread_rng();
+    let mut random: u8 = rng.gen();
+    random %= 101;
+    // println!("range 0~100 value {}", random);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u8 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("number only!");
+                continue
+            }
+        };
+        println!("guess: {}", guess);
+        match guess.cmp(&random) {
+            std::cmp::Ordering::Less => println!("Too Small!"),
+            std::cmp::Ordering::Greater => println!("Too Big!"),
+            std::cmp::Ordering::Equal => {
+                println!("Coool! You Win!");
+                break;
+            }
+        }
+    }
 }
